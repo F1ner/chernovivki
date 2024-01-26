d94a14bd8321d8a0d7f4d70bddbe564cf0f92597
F1ner <123871318+F1ner@users.noreply.github.com>
2024-01-26 14:22:36 +0200

    16.1

---
+++ Ababiy16_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //DateTime date1 = DateTime.Now;
+            //DateTime today = DateTime.Now;
+            //Console.WriteLine("Текущая дата:");
+            //Console.WriteLine(date1.ToString("d dd ddd dddd"));
+            //Console.WriteLine($"{date1.ToShortDateString()}");
+            //Console.WriteLine($"{date1.Day} янв {date1.Year}");
+            //Console.WriteLine($"{date1.Day} января {date1.Year} г");
+            //Console.WriteLine(date1.ToString($"{date1.DayOfWeek},{date1.Day} янв {date1.Year}"));
+            //Console.WriteLine($"");
+            //Console.WriteLine($"");
+            //Console.WriteLine($"");
+
+            //2
+            //DateTime today = DateTime.Now;
+            //Console.WriteLine("Введите возраст человека:");
+            //int age = int.Parse(Console.ReadLine());
+            //int year = today.Year-today.Month-age;
+            //Console.WriteLine($"Ты родился в {year} году");
+
+            //3
+            //DateTime today = DateTime.Now;
+            //Console.WriteLine("Введите год рождения человека:");
+            //DateTime date;
+            //DateTime.TryParse(Console.ReadLine(), out date);
+            // int age = today.Year-today.Month-date.Year;
+            //Console.WriteLine($"Этому человеку {age} лет");
+
+
+            Console.ReadLine();
+        }
+    }
+}
