415d13a31a3c4d43ad286dff72a9360e19d5f387
F1ner <123871318+F1ner@users.noreply.github.com>
2024-01-29 09:48:45 +0200

    Add files via upload

---
+++ Ababiy16_3/DateClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy16_3
+{
+    class DateClass
+    {
+        DateTime date;
+     
+       
+        public DateClass() { }
+
+        public DateClass(int year, int month, int day)
+        {
+            //this.year = year;
+            //this.month = month;
+            //this.day = day;
+            date = new DateTime(year, month, day);
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set{ date = value; }
+        }
+
+     
+
+        public DateTime Yesterday()
+        {
+            return Date.AddDays(-1);
+
+
+            //Console.WriteLine("Дата:");
+            //DateTime date1;
+            //DateTime.TryParse(Console.ReadLine(), out date);
+
+
+        }
+
+        public DateTime Tomorrow()
+        {
+            return Date.AddDays(1);
+
+            //Console.WriteLine("Дата:");
+            //DateTime date2;
+            //DateTime.TryParse(Console.ReadLine(), out date);
+
+
+        }
+
+      
+        public int EndMonth() 
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month)-date.Day;
+
+        }
+
+
+        
+    }
+
+    }
+
---
+++ Ababiy16_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy16_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateClass date = new DateClass();
+            
+
+
+            Console.WriteLine(date);
+
+            Console.ReadLine();
+        }
+    }
+}
